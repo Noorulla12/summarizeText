9b3b23863ec19c6abc96f26ad55d08d869e2b1da
rupan <rupan@SSSOLVEM2800>
2024-07-17 15:58:30 +0530

    Add project files.

---
+++ summarizeText/Program.cs
@@ -0,0 +1,19 @@
+﻿using summarizeText;
+using System;
+using System.Collections.Generic;
+
+namespace SummarizeText
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            var sentence = " This is going to be a really really really really really really long text.";
+
+            var summary = StringUtility.SummarizingText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+    }
+   
+}
